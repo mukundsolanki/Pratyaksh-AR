cb34671483a716d45124cc32cecfa0b241df90d6
Mukund Solanki <mukundsolanki665@gmail.com>
2023-11-04 23:14:49 +0530

    Dynamically IP Parse sucessful

--- pratyakshapp/lib/settings_page.dart
+++ pratyakshapp/lib/settings_page.dart
@@ -1,24 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [Colors.black, Color.fromARGB(255, 49, 49, 49)],
           ),
         ),
-        child: Center(
-          child: Text(
-            'This is the Settings Page',
-            style: TextStyle(fontSize: 24),
-          ),
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  child: Image.asset(
+                    'assets/banner.jpg',
+                    fit: BoxFit.cover,
+                    // height: 100,
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: CircularPercentIndicator(
+                        radius: 100,
+                        lineWidth: 20,
+                        percent: 0.69,
+                        progressColor: Color.fromARGB(255, 65, 227, 168),
+                        arcType: ArcType.FULL,
+                        arcBackgroundColor: Color.fromARGB(255, 54, 54, 54),
+                        circularStrokeCap: CircularStrokeCap.round,
+                        center: Text(
+                          '69%',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    _buildInfoRow("Device Name", "Pratyaksh AR"),
+                    _buildInfoRow("Connectivity Status", "Connected"),
+                    _buildInfoRow("Device Firmware", "v1.0.0"),
+                    _buildInfoRow("IP Address", "192.168.1.1"),
+                    _buildInfoRow("MAC Address", "AA:BB:CC:DD:EE:FF"),
+                    SizedBox(width: 10),
+                    _buildUnlinkButton(),
+                    // Add more info rows as needed
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Widget _buildInfoRow(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+                color: Colors.white),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+                fontSize: 16.0, color: Color.fromARGB(255, 242, 242, 242)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildUnlinkButton() {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Center(
+      child: ElevatedButton(
+        onPressed: () {
+          // Implement your unlink device logic here
+        },
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          primary: const Color.fromARGB(255, 200, 13, 0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.link_off,
+                color: Colors.white,
+              ),
+              SizedBox(width: 8.0),
+              Text(
+                "Unlink Device",
+                style: TextStyle(fontSize: 16.0, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
--- pratyakshapp/lib/welcome_screen.dart
+++ pratyakshapp/lib/welcome_screen.dart
@@ -3,150 +3,121 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   final Function(String) onIPSubmit;
 
-  const WelcomeScreen({super.key, required this.onIPSubmit});
+  const WelcomeScreen({Key? key, required this.onIPSubmit}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     String ipAddress = '';
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        elevation: 0,
-        title: const Center(
-          child: Text(
-            'DeafApp',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 65, 227, 168),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/welcome.jpg',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.black, Color.fromARGB(255, 49, 49, 49)],
-          ),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Center(
-              child: SingleChildScrollView(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[900],
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        blurRadius: 10,
-                        offset: const Offset(0, 4),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Align text to the left
+                  children: [
+                    Text(
+                      'Welcome',
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      const Text.rich(
-                        TextSpan(
-                          text: 'Welcome to ',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'DeafApp',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 65, 227, 168),
+                    ),
+                    SizedBox(height: 10.0), // Spacer
+                    // Input Field and Button Side by Side
+                    Row(
+                      children: [
+                        // Input Field
+                        Expanded(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            onChanged: (value) {
+                              ipAddress = value;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Enter your unique code',
+                              labelStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(color: Colors.white),
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          onChanged: (value) {
-                            ipAddress = value;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                              ),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                              ),
-                            ),
-                            labelText: ' Enter your unique code',
-                            labelStyle: TextStyle(color: Colors.white),
-                          ),
-                          style: const TextStyle(
-                            color: Colors.white,
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 16),
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 30,
-                        child: IconButton(
-                          onPressed: () {
-                            onIPSubmit(ipAddress);
-                          },
-                          icon: const Icon(
-                            Icons.arrow_forward,
-                            color: Colors.black,
+                        SizedBox(
+                            width:
+                                20.0),
+                        // Button
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 30,
+                          child: IconButton(
+                            onPressed: () {
+                              onIPSubmit(ipAddress);
+                            },
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.black,
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 32),
-                      RichText(
-                        text: const TextSpan(
+                      ],
+                    ),
+                    SizedBox(height: 15.0),
+                    Center(
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
                           children: [
                             TextSpan(
                               text: 'Created with ☕ by ',
                               style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
                                 color: Colors.white,
+                                fontStyle: FontStyle.italic,
                               ),
                             ),
                             TextSpan(
                               text: 'SyntaxSorcerers',
                               style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
                                 color: Color.fromARGB(255, 65, 227, 168),
+                                fontStyle: FontStyle.italic,
                               ),
                             ),
                           ],
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    ),
+                    SizedBox(height: 150.0),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

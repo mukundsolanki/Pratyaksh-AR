3047e81b9e8b5af4a57191e934138334219e595c
Mukund Solanki <mukundsolanki665@gmail.com>
2023-11-04 16:39:22 +0530

    Added the functionality to add the tasks in the app

--- pratyakshapp/lib/home_page.dart
+++ pratyakshapp/lib/home_page.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String ipAddress;
 
   HomePage({required this.ipAddress});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> tasks = [];
+  TextEditingController _taskController = TextEditingController();
+
+  void _addTask() {
+    String task = _taskController.text;
+    if (task.isNotEmpty) {
+      setState(() {
+        tasks.add(task);
+        _taskController.clear();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,21 +49,62 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Your Unique Code is:',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Color.fromARGB(255, 65, 227, 168),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(18),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: _taskController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Enter your task',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: _addTask,
+                      child: Icon(Icons.add),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green),
+                            shape: MaterialStateProperty.all<OutlinedBorder>(CircleBorder()),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                ipAddress,
-                style: TextStyle(fontSize: 18, color: Colors.white),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: tasks.map((task) {
+                  return Text(
+                    '- $task',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  );
+                }).toList(),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
   }
 }

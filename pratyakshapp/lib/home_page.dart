6b2dfbf5ce127b47966884ddae8c948347936763
Mukund Solanki <mukundsolanki665@gmail.com>
2023-11-04 17:37:02 +0530

    Bottom nav bar widget in home page

--- pratyakshapp/lib/home_page.dart
+++ pratyakshapp/lib/home_page.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
+import 'about_page.dart';
+import 'settings_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeafApp',
+      home: HomePage(ipAddress: '127.0.0.1'), // Example IP address
+    );
+  }
+}
 
 class HomePage extends StatefulWidget {
   final String ipAddress;
@@ -12,12 +28,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<String> tasks = [];
   TextEditingController _taskController = TextEditingController();
+  int _currentIndex = 0;
 
   void _addTask() {
     String task = _taskController.text;
     if (task.isNotEmpty) {
       setState(() {
-        tasks.insert(0, task); // Insert task at the beginning of the list
+        tasks.insert(0, task);
         _taskController.clear();
       });
     }
@@ -25,7 +42,7 @@ class _HomePageState extends State<HomePage> {
 
   void _deleteTask(int index) {
     setState(() {
-      tasks.removeAt(index); // Remove task at the specified index
+      tasks.removeAt(index);
     });
   }
 
@@ -43,94 +60,134 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.black, Color.fromARGB(255, 49, 49, 49)],
-          ),
+      body: _currentIndex == 0
+          ? _buildHomeContent()
+          : _currentIndex == 1
+              ? AboutPage()
+              : SettingsPage(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(12.0),
+          topRight: Radius.circular(12.0),
         ),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      color: Colors.black45,
-                      margin: EdgeInsets.all(10),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.voice_chat,
-                              color: Color.fromARGB(255, 65, 227, 168),
-                            ),
-                            Expanded(
-                              child: Text(
-                                ' ${tasks[index]}',
-                                style: TextStyle(
-                                    fontSize: 18, color: Colors.white),
-                              ),
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () => _deleteTask(index),
-                              color: Colors.red,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          selectedItemColor: Color.fromARGB(255, 65, 227, 168),
+          unselectedItemColor: Colors.white,
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-            Container(
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.all(18),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _taskController,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: 'Enter your Wake Word',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: _addTask,
-                    child: Icon(Icons.add),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Color.fromARGB(255, 65, 227, 168),),
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                        CircleBorder(),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info),
+              label: 'About',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildHomeContent() {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.black, Color.fromARGB(255, 49, 49, 49)],
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.black45,
+                    margin: EdgeInsets.all(10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.voice_chat,
+                            color: Color.fromARGB(255, 65, 227, 168),
+                          ),
+                          Expanded(
+                            child: Text(
+                              ' ${tasks[index]}',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () => _deleteTask(index),
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _taskController,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Enter your Wake Word',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _addTask,
+                  child: Icon(Icons.add),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 65, 227, 168),
+                    ),
+                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                      CircleBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

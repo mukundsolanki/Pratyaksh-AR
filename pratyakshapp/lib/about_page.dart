586bc199a506db40a4b87e4b5286c853a7a7110b
Mukund Solanki <mukundsolanki665@gmail.com>
2023-11-04 17:05:44 +0530

    Sample About page added'

---
+++ pratyakshapp/lib/about_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the About Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}

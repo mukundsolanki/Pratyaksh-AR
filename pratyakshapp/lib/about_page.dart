4bf73fa06f24874cecd22f0101abae970f9f963e
Mukund Solanki <mukundsolanki665@gmail.com>
2023-11-04 18:54:57 +0530

    Linear background as home screen added

--- pratyakshapp/lib/about_page.dart
+++ pratyakshapp/lib/about_page.dart
@@ -4,13 +4,19 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('About'),
-      ),
-      body: Center(
-        child: Text(
-          'This is the About Page',
-          style: TextStyle(fontSize: 24),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black, Color.fromARGB(255, 49, 49, 49)],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'This is the About Page',
+            style: TextStyle(fontSize: 24),
+          ),
         ),
       ),
     );
